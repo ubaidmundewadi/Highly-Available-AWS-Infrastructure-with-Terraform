63c0a9d4790ccd70d767881bca18badff7262d64
Ubaid Mundewadi <137920062+ubaidmundewadi@users.noreply.github.com>
2024-12-23 11:48:23 +0530

    map_public_ip argument added

--- vpc.tf
+++ vpc.tf
@@ -11,6 +11,7 @@ module "vpc" {
   single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
+  map_public_ip_on_launch = true    # This will assign public ip to instances created in public subnet
 
   tags = {
     Terraform   = "true"
